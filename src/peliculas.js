52ec912bd670cb3af96e18221a6be4e8c71df1be
mig183 <epp34339@gmail.com>
2026-09-20 20:23:50 -0500

    Add files via upload

---
+++ src/peliculas.js
@@ -0,0 +1,23 @@
+export const misPeliculas = {
+    667520: {
+         name: "Amor de gata",
+         videoUrl: "https://old.bitchute.com/api/beta9/embed/YDG5zDzxFGKT/?videoID=YDG5zDzxFGKT&startTime=0&autoPlay=false&theaterMode=false&showAds=true&embedMode=false&rel=0"
+     },
+    1204680: {
+         name: "Coyote vs acme",
+         videoUrl: "https://voe.sx/e/dfkhmnj5bs2r"
+     },
+    14411: {
+         name: "simbad la leyenda de los 7 mares",
+         videoUrl: '<iframe src="https://www.tokyvideo.com/es/embed/868348" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency style="display:block;max-width:100%;margin:0 auto 10px"></iframe>'
+     },
+    969681: {
+         name: "spiderman un nuevo dia",
+         videoUrl: 'https://cinemaos.tech/player/969681?theme=ffffff'
+     },
+    1368337: {
+         name: "La odisea 2026",
+         videoUrl: 'https://cinemaos.tech/player/1368337?theme=ffffff&autoPlay=true'
+     }
+
+};
